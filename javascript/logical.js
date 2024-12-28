aa96dd5e5c12da453e9e8a085d2bcc8fbd6a9bb0
Sharan-Prasath <sharanprasaths.22cse@kongu.edu>
2024-12-28 22:23:58 +0530

    ver2

---
+++ javascript/logical.js
@@ -0,0 +1,12 @@
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a)
+console.log(a & b);
+console.log(a | b);
+console.log(a ^ b);
+console.log(~a);
+console.log(a << 1);
+console.log(a >> 1);
+console.log(a >>> 1);
